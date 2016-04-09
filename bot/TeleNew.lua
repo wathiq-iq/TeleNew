d3c4fcf842adb9c59d079cd15df550c15f6f190b
Wathiq Al-Qajar <wathiq-iq@users.noreply.github.com>
2016-04-09 12:24:34 +0300

    Update and rename seedbot.lua to TeleNew.lua

--- bot/TeleNew.lua
+++ bot/TeleNew.lua
@@ -213,51 +213,58 @@ function create_config( )
   config = {
     enabled_plugins = {
 	"admin",
-    "onservice",
     "inrealm",
     "ingroup",
     "inpm",
-    "banhammer",
     "stats",
-    "anti_spam",
     "owners",
-    "arabic_lock",
     "set",
     "get",
     "broadcast",
     "invite",
     "all",
     "leave_ban",
-	"supergroup",
-	"whitelist",
-	"msg_checks"
+    "supergroup",
+    "msg_checks",
+    "plugins",
+    "help2",
+    "helpme",
+    "help",
+    "textphoto",
+    "taks",
+    "voice",
+    "welcome",
+    "onxerviec",
+    "me",
+    "insta",
+    "Feedback",
+    "commands",
+    "addsudo",
+    "Version",
+    "arbic_lock",
+    "anti-spam",
+    "replay",
+    "banhammer",
+    "sticker",
+    "block",
+    "super-info",
+    "getlink",
+    "renk",
+    "id2",
+    "image"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {127876523,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[TeleNeW v8
 An advanced administration bot based on TG-CLI written in Lua
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
+#Developers
+@wawi8 [ Dev ]
+@hunter18k  [ Dev ]
+@zainahmed98  [ Dev ]
 Our channels
-@teleseedch [English]
-@iranseed [persian]
-
-Our website 
-http://teleseed.seedteam.org/
+@iq_dev8 [ Arabic ]
+http://telegram.me/iq_dev8
+The github <>\ git clone https://github.com/wawi8/TeleNew.git -b supergroups
 ]],
     help_text_realm = [[
 Realm Commands:
